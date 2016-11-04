8e081b58fd6e621901624793c13a770b75774870
scizeron <stephane.cizeron@gmail.com>
2016-11-04 12:41:04 +0100

    adjustments

--- oauth2.js
+++ oauth2.js
@@ -1,91 +1,94 @@
 var oauth2Utils = {
  'guid' : function() {
-  function s4() {
-   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+   function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+   }
+   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
  },
- 'newSessionItem' : function( value, ttl) {
-  var item;
-  if (typeof(ttl) != "undefined") {
-   item = {
-    v: value, 
-    t: new Date().getTime(), 
-    l: ttl * 1000
-   };
-  } else {
-   item = {
-    v: value
-   };
-  }
-  return item;
+ 'newSessionItem' : function(value, ttl) {
+   var item;
+   if (typeof(ttl) != "undefined") {
+    item = {
+     v: value, 
+     t: new Date().getTime(), 
+     l: ttl * 1000
+    };
+   } else {
+    item = {
+     v: value
+    };
+   }
+   return item;
  },
  'getParameter' : function(url, name) {
-  name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
-  var regexS = '[\\#&?/]' + name + '=([^&#]*)';
-  var regex = new RegExp(regexS);
-  var results = regex.exec(url);
-  if (results != null) {
-   return results[1];
-  }
-  return null;
+   name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+   var regexS = '[\\#&?/]' + name + '=([^&#]*)';
+   var regex = new RegExp(regexS);
+   var results = regex.exec(url);
+   if (results != null) {
+    return results[1];
+   }
+   return null;
  },
  'debug' : function(msg) {
-  if (typeof(console) != "undefined") {
-   console.log(msg);
-  }
+   if (typeof(console) != "undefined") {
+    console.log(msg);
+   }
  }, 
  'error' : function(msg) {
-  if (typeof(console) != "undefined") {
-   console.error(msg);
-  }
+   if (typeof(console) != "undefined") {
+    console.error(msg);
+   }
  },
  'setItem' : function(key, value) {
-  var item = oauth2Utils.newSessionItem(value);
-  var jsonItem = JSON.stringify(item);
-  localStorage.setItem(key, jsonItem);
+   var item = oauth2Utils.newSessionItem(value);
+   var jsonItem = JSON.stringify(item);
+   localStorage.setItem(key, jsonItem);
  },
  'getItem' : function(key) {
-  var item;
-  var jsonItem = localStorage.getItem(key);
-  if (jsonItem == null) {
-   return null;
-  }
-  return JSON.parse(jsonItem).v;
+   var item;
+   var jsonItem = localStorage.getItem(key);
+   if (jsonItem == null) {
+    return null;
+   }
+   return JSON.parse(jsonItem).v;
  }, 
  'setSessionItem' : function(key, value, ttl) {
-  var item = oauth2Utils.newSessionItem(value, ttl);
-  var jsonItem = JSON.stringify(item);
-  sessionStorage.setItem(key, jsonItem);
+   var item = oauth2Utils.newSessionItem(value, ttl);
+   var jsonItem = JSON.stringify(item);
+   sessionStorage.setItem(key, jsonItem);
  },
  'getSessionItem' : function(key) {
-  var item;
-  var jsonItem = sessionStorage.getItem(key);
-  if (jsonItem == null) {
+   var item;
+   var jsonItem = sessionStorage.getItem(key);
+   if (jsonItem == null) {
+    return null;
+   }
+   item = JSON.parse(jsonItem);
+   if (typeof(item.l) == "undefined") {
+    return item.v;
+   }
+   if ((item.t + item.l) > new Date().getTime() ) {
+    return item.v;
+   }
+   oauth2Utils.debug("\"" + key + "\" is expired");
+   oauth2Utils.removeSessionItem(key);
    return null;
-  }
-  item = JSON.parse(jsonItem);
-  if (typeof(item.l) == "undefined") {
-   return item.v;
-  }
-  if ((item.t + item.l) > new Date().getTime() ) {
-   return item.v;
-  }
-  oauth2Utils.debug("\"" + key + "\" is expired");
-  oauth2Utils.removeSessionItem(key);
-  return null;
  },
  'removeSessionItem' : function(key) {
-  sessionStorage.removeItem(key);
+   sessionStorage.removeItem(key);
  },
  'clearSession' : function() {
-  sessionStorage.clear();
- }
+   sessionStorage.clear();
+ },
+ 'getAccessToken' : function(clientId) {
+   return getSessionItem(clientId  + '.oauth2.accessToken');
+ }   
 };
 
 var oauth2 = {
  'getAccessToken' : function(clientId, successCallback, errorCallback) {
-   var accessToken = oauth2Utils.getSessionItem(clientId +'.oauth2.accessToken');
+   var accessToken = oauth2Utils.getAccessToken(clientId);
    var accessTokenMode = 'session';
    var stateOk = true;
    var expiresIn = null;
@@ -118,6 +121,7 @@ var oauth2 = {
     stateParameter = oauth2Utils.getParameter(window.location, 'state');
     stateSession = oauth2Utils.getSessionItem(clientId + '.oauth2.state'); 
     oauth2Utils.removeSessionItem(clientId + '.oauth2.state');
+    
     // IE issue, stateSession may be null ...
     if (stateSession != null && stateParameter != stateSession) {
      oauth2Utils.error(stateParameter + '<> ' + stateSession);
@@ -158,7 +162,7 @@ var oauth2 = {
   oauth2Utils.debug('---------------------------------------------------------------');
   oauth2Utils.debug('  authorize');
   oauth2Utils.debug('---------------------------------------------------------------');
-  oauth2Utils.debug('- uri           : ' + url);
+  oauth2Utils.debug('- request       : ' + url);
   oauth2Utils.debug('- redirect_uri  : ' + redirectUri);
   oauth2Utils.debug('---------------------------------------------------------------');
     
