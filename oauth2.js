22ce0615f7033c71bea6cb94b3cd9198a4c79345
scizeron <stephane.cizeron@gmail.com>
2016-12-14 09:48:17 +0000

    update

--- oauth2.js
+++ oauth2.js
@@ -58,6 +58,11 @@ var oauth2Utils = {
    var jsonItem = JSON.stringify(item);
    sessionStorage.setItem(key, jsonItem);
  },
+ 'setSessionItemInStorage' : function(storage, key, value, ttl) {
+   var item = oauth2Utils.newSessionItem(value, ttl);
+   var jsonItem = JSON.stringify(item);
+   storage.setItem(key, jsonItem);
+ }, 
  'getSessionItem' : function(key) {
    var item;
    var jsonItem = sessionStorage.getItem(key);
@@ -85,14 +90,30 @@ var oauth2Utils = {
    return oauth2Utils.getSessionItem(clientId  + '.oauth2.accessToken');
  },
  'removeAccessToken' : function(clientId) {
-   return oauth2Utils.removeItem(clientId  + '.oauth2.accessToken');
+   return oauth2Utils.removeSessionItem(clientId  + '.oauth2.accessToken');
  }      
 };
 
 var oauth2 = {
+ 'getExpiresInSecs' : function(clientId) {
+   var jsonItem = sessionStorage.getItem(clientId + '.oauth2.accessToken');
+   var item;
+   if (jsonItem == null) {
+    return 0;	   
+   }
+   item = JSON.parse(jsonItem);
+   if (typeof(item.l) == "undefined") {
+	return 0;
+   }
+   var remainingTimeInMillis = item.t + item.l - new Date().getTime();
+   if (remainingTimeInMillis > 0) {
+    return remainingTimeInMillis / 1000;
+   }
+   return 0;
+  },
  'getAccessToken' : function(clientId, successCallback, errorCallback) {
    var accessToken = oauth2Utils.getAccessToken(clientId);
-   var accessTokenMode = 'session';
+   var accessTokenOrigin = 'session';
    var stateOk = true;
    var expiresIn = null;
    var stateParameter;
@@ -102,7 +123,7 @@ var oauth2 = {
    
    if (accessToken == null) {
     accessToken = oauth2Utils.getParameter(window.location, 'access_token');
-    accessTokenMode = 'request';
+    accessTokenOrigin = 'request';
    }
      
    if (accessToken == null) {
@@ -118,16 +139,16 @@ var oauth2 = {
 	return null;   
    }
    
-   oauth2Utils.debug('use access_token from '  + accessTokenMode + ' : ' + accessToken);
+   oauth2Utils.debug('use access_token from '  + accessTokenOrigin + ' : ' + accessToken);
     
-   if (accessTokenMode == 'request') {
+   if (accessTokenOrigin == 'request') {
     stateParameter = oauth2Utils.getParameter(window.location, 'state');
     stateSession = oauth2Utils.getSessionItem(clientId + '.oauth2.state'); 
     oauth2Utils.removeSessionItem(clientId + '.oauth2.state');
     
     // IE issue, stateSession may be null ...
     if (stateSession != null && stateParameter != stateSession) {
-     oauth2Utils.error(stateParameter + '<> ' + stateSession);
+     oauth2Utils.error(stateSession + ' <> ' + stateParameter);
      oauth2Utils.error('The state validation has failed [expected: ' + stateSession + ', received: ' + stateParameter + '], it\'s a security problem !!!');
      if (typeof(errorCallback) != 'undefined') {
        oauth2Utils.debug('Call errorCallback ...');
@@ -138,39 +159,51 @@ var oauth2 = {
    
     expiresIn = oauth2Utils.getParameter(window.location, 'expires_in');
     oauth2Utils.setSessionItem(clientId + '.oauth2.accessToken', accessToken, expiresIn);
+    oauth2Utils.removeSessionItem(clientId + '.oauth2.accessToken.tmp');
+    
+   } else if (accessTokenOrigin == 'session') {
+	expiresIn = oauth2.getExpiresInSecs(clientId); 
    }
    
    if (typeof(successCallback) != 'undefined') {
    	 oauth2Utils.debug('Call successCallback ...');
-   	 successCallback(accessToken, expiresIn);   
+   	 successCallback(accessToken, expiresIn, accessTokenOrigin);   
    }
    
    return accessToken;
  },
+ 'getAuthorizeRequest' : function(clientId, scopes, authorizeUrl, redirectUri) {
+   var expiresIn = oauth2.getExpiresInSecs(clientId);
+   var accessToken = oauth2Utils.getSessionItem(clientId + '.oauth2.accessToken');
+   oauth2Utils.removeAccessToken(clientId);
+   oauth2Utils.setSessionItem(clientId + '.oauth2.accessToken.tmp', accessToken, expiresIn * 1000);
+   
+   var state = oauth2Utils.guid();
+   oauth2Utils.setSessionItem(clientId + '.oauth2.state', state);
+   
+   var url = authorizeUrl + '?client_id=' + encodeURIComponent(clientId) 
+	 	  				  + '&response_type=token'
+	 	  				  + '&state=' + encodeURIComponent(state)
+	 	  				  ;
+  
+   if (scopes != '') {
+    url = url + '&scope=' + encodeURIComponent(oauth2Cfg.scopes);
+   } else {
+    oauth2Utils.debug('no scope is requested');  
+   }
+   
+   url = url + '&redirect_uri=' + encodeURIComponent(redirectUri)
+   
+   oauth2Utils.debug('---------------------------------------------------------------');
+   oauth2Utils.debug('  authorize');
+   oauth2Utils.debug('---------------------------------------------------------------');
+   oauth2Utils.debug('- request       : ' + url);
+   oauth2Utils.debug('- redirect_uri  : ' + redirectUri);
+   oauth2Utils.debug('---------------------------------------------------------------');
+    
+   return url;
+ },
  'authorize' : function(clientId, scopes, authorizeUrl, redirectUri) {
-  oauth2Utils.removeAccessToken(clientId);
-  var state = oauth2Utils.guid();
-  var url = authorizeUrl + '?client_id=' + encodeURIComponent(clientId) 
-		  				 + '&response_type=token'
-		  				 + '&state=' + encodeURIComponent(state)
-		  				 + '&redirect_uri=' + encodeURIComponent(redirectUri);
-  
-  if (scopes != '') {
-   url = url + '&scope=' + encodeURIComponent(oauth2Cfg.scopes);
-  } else {
-   oauth2Utils.debug('no scope is requested');  
-  }
-  
-  oauth2Utils.setSessionItem(clientId + '.oauth2.state', state);
-  
-  oauth2Utils.debug('---------------------------------------------------------------');
-  oauth2Utils.debug('  authorize');
-  oauth2Utils.debug('---------------------------------------------------------------');
-  oauth2Utils.debug('- request       : ' + url);
-  oauth2Utils.debug('- redirect_uri  : ' + redirectUri);
-  oauth2Utils.debug('---------------------------------------------------------------');
-    
-  window.location.replace(url);  
-    
- }
+   window.location.replace(oauth2.getAuthorizeRequest(clientId, scopes, authorizeUrl, redirectUri, prompt));  
+ } 
 }
